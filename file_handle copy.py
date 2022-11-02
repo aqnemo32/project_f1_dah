da864b6c6329cc946a07e4a4751e31418acfb770
aqnemo32 <s1973088@ed.ac.uk>
2022-11-02 21:52:59 +0000

    Update file_handle copy.py

--- file_handle copy.py
+++ file_handle copy.py
@@ -65,36 +65,36 @@ def main():
 
     #histogram of the peaks from raw data
     bins_1 = []
-    counts_1 = []
+    count_1 = []
     bins_back = []
-    counts_back= []
+    count_back= []
     bins_2 = []
-    counts_2 = []
+    count_2 = []
     bins_3 = []
-    counts_3 = []
+    count_3 = []
 
     # setting up the peak and background parts of the histogram
     for i,j in zip(bins, count):
         if i > 9.25 and i <9.7:
             bins_1.append(i)
-            counts_1.append(j)
+            count_1.append(j)
         elif i < 9.2 or i > 10.55:
             bins_back.append(i)
-            counts_back.append(j)
+            count_back.append(j)
         elif i < 9.8 and i > 9.7:
             bins_back.append(i)
-            counts_back.append(j)
+            count_back.append(j)
         elif i > 9.85 and i <10.15:
             bins_2.append(i)
-            counts_2.append(j)
+            count_2.append(j)
         elif i > 10.25 and i <10.45:
             bins_3.append(i)
-            counts_3.append(j)
+            count_3.append(j)
     
-    # print(f'{len(bins_1)}\n{len(counts_1)}')
+    # print(f'{len(bins_1)}\n{len(count_1)}')
     # fitting the exponential decay of the background count
         
-    param_back, cov_back = curve_fit(decay, bins_back, counts_back)
+    param_back, cov_back = curve_fit(decay, bins_back, count_back)
 
     # back_sub = decay(np.array(bins_back), param_back[0], param_back[1])
     # plt.scatter(bins_back, back_sub, color = 'r')
@@ -102,16 +102,16 @@ def main():
     # plt.show()
     # plt.clf()
     
-    counts_clean = np.array(count) - decay(np.array(bins[1:]), param_back[0], param_back[1])
+    count_clean = np.array(count) - decay(np.array(bins[1:]), param_back[0], param_back[1])
     
-    counts_1_clean = np.array(counts_1) - decay(np.array(bins_1), param_back[0], param_back[1])
-    counts_2_clean = np.array(counts_2) - decay(np.array(bins_2), param_back[0], param_back[1])
-    counts_3_clean = np.array(counts_3) - decay(np.array(bins_3), param_back[0], param_back[1])
+    count_1_clean = np.array(count_1) - decay(np.array(bins_1), param_back[0], param_back[1])
+    count_2_clean = np.array(count_2) - decay(np.array(bins_2), param_back[0], param_back[1])
+    count_3_clean = np.array(count_3) - decay(np.array(bins_3), param_back[0], param_back[1])
     
     
-    param_1, cov_1 = curve_fit(gauss, bins_1, counts_1_clean, p0 = [15000, 0.3,9.5])
-    param_2, cov_2 = curve_fit(gauss, bins_2, counts_2_clean, p0 = [5000, 0.3,10.1])
-    param_3, cov_3 = curve_fit(gauss, bins_3, counts_3_clean, p0 = [2500, 0.3,10.35])
+    param_1, cov_1 = curve_fit(gauss, bins_1, count_1_clean, p0 = [15000, 0.3,9.5])
+    param_2, cov_2 = curve_fit(gauss, bins_2, count_2_clean, p0 = [5000, 0.3,10.1])
+    param_3, cov_3 = curve_fit(gauss, bins_3, count_3_clean, p0 = [2500, 0.3,10.35])
     
     count_1_fit = gauss(bins_1, param_1[0], param_1[1], param_1[2]) + decay(
         np.array(bins_1), param_back[0], param_back[1])
@@ -131,12 +131,21 @@ def main():
     
     #Residual
     
-    res_1 = count_1_fit - counts_1
-    res_2 = count_2_fit - counts_2
-    res_3 = count_3_fit - counts_3
+    res_1 = count_1_fit - count_1
+    res_1_sq = np.square(res_1)
+    res_2 = count_2_fit - count_2
+    res_3 = count_3_fit - count_3
     
+    r_sq_1 = 1 - (np.sum((count_1- count_1_fit)**2)/(np.sum((count_1-np.mean(count_1))**2)))
+    print(r_sq_1)
     plt.scatter(bins_1,res_1, marker = 'x', lw = 0.8, color = 'k')
     plt.title('Residual plot for peak 1')
+    plt.xlabel('Mass')
+    plt.ylabel('Residual')
+    plt.show()
+    plt.clf()
+    plt.scatter(bins_1,res_1_sq, marker = 'x', lw = 0.8, color = 'k')
+    plt.title('Residual squared plot for peak 1')
     plt.xlabel('Mass')
     plt.ylabel('Residual')
     plt.show()
