29743cfa5fa38f9d0bd28da3b7041ff8047cb247
aqnemo32 <s1973088@ed.ac.uk>
2022-11-04 18:25:12 +0000

    Update mc_anal.py
    made the double_gauss fit work, a bit annoying that there is a kink around 9.25, need to look into it

--- mc_anal.py
+++ mc_anal.py
@@ -22,7 +22,7 @@ def main():
     # b = datetime.datetime.now() - a
     # print(b)
     count , bins, patches = plt.hist(xmass, color = 'k', bins = 600, histtype= 'bar', range =(Min, Max), density=True )
-
+    plt.clf()
 
     #histogram of the peaks from raw data
     bins_1 = []
@@ -75,32 +75,54 @@ def main():
     std_1 = param_1[1]
     print(f"{mean_1=}\n{std_1=}")
     # why does changing the limits of peak 1 means that the tail center split not work anymore
-    tail = []
-    count_tail = []
+
+    tail_1st = []
+    count_tail_1st = []
+    tail_2nd = []
+    count_tail_2nd = []
+
     center = []
     count_center = []
+    #defines the width of the 'center' region
+    w = 5
+    low_lim = mean_1 - w*std_1
+    up_lim = mean_1 + w*std_1
     # print(bins_1, count_1)
     
     for i,j in zip(bins_1, count_1):
-        if i > mean_1-2*std_1 and i < mean_1+2*std_1:
-
+        if i > low_lim and i < up_lim:
             center.append(i)
             count_center.append(j)
-        elif i < param_1[2]-2*param_1[1] or i > param_1[2]+2*param_1[1]:
-
-            tail.append(i)
-            count_tail.append(j)
-    param_tail, cov_t = curve_fit(gauss, tail, count_tail, p0 = [4, 0.1, 9.45])
+            
+        elif i < low_lim:
+            tail_1st.append(i)
+            count_tail_1st.append(j)
+            
+        elif i> up_lim:
+            tail_2nd.append(i)
+            count_tail_2nd.append(j)
+            
+    tail_1st = np.array(tail_1st)
+    tail_2nd = np.array(tail_2nd)
+    tail = np.concatenate((tail_1st, tail_2nd))
+    count_tail = np.concatenate((count_tail_1st, count_tail_2nd))
+    param_tail, cov_t = curve_fit(gauss, tail, count_tail, p0 = [5, 0.1, 9.45], maxfev = 1600)
     param_center, cov_c = curve_fit(gauss, center, count_center, p0= [10, 0.04, 9.45])
     
     
-    tail_fit = gauss(tail, param_tail[0], param_tail[1], param_tail[2])
+    tail_fit = gauss(np.concatenate((tail_1st, tail_2nd)), param_tail[0], param_tail[1], param_tail[2])
     center_fit = gauss(center, param_center[0], param_center[1], param_center[2])
-    # print(tail)
-    tail_1st_index = tail_fit < 9.5
-    split_of_tail_ends = len(tail_1st_index)-1
-    tail_1st_half = tail[:split_of_tail_ends]
-    tail_2nd_half = tail[split_of_tail_ends:]
+    # print(f'{tail_fit=}')
+    # print(f'{param_1[2]=}'
+    print(f'{tail_1st=}\n{center=}\n{tail_2nd=}')
+    print(f'{bins_1=}')
+    print(f'{len(tail_1st)=}\n{len(tail_2nd)=}\n{len(center)=}\n{len(bins_1)=}')
+    print(f'{len(tail)+len(center)=}')
+    double_gauss_fit = np.concatenate((tail_fit[:len(tail_1st)], center_fit, tail_fit[len(tail_1st):]))
+    
+    plt.plot(bins_1, double_gauss_fit)
+    plt.show()
+    plt.clf()
     # need to find a way of combining the tail and center of the double gaussian fit
     # double_gauss_fit = np.array([gauss(tail_1st_half, param_tail[0], param_tail[1], param_tail[2]),
     #                              gauss(center, param_tail[0], param_tail[1], param_tail[2]),
@@ -112,11 +134,11 @@ def main():
     # possible using 
 
     # print(center)
-    param_1_d, cov_1_d = curve_fit(
-        lambda x, a_1, mu_1, sig_1, a_2, mu_2, sig_2: 
-        double_gauss(x, a_1, mu_1, sig_1, a_2, mu_2, sig_2, param_1[0], param_1[1]),
-        bins_1, count_1,
-        p0 = [10, 0.2, 9.45, 2, 0.4, 9.45], maxfev = 4000)
+    # param_1_d, cov_1_d = curve_fit(
+    #     lambda x, a_1, mu_1, sig_1, a_2, mu_2, sig_2: 
+    #     double_gauss(x, a_1, mu_1, sig_1, a_2, mu_2, sig_2, param_1[0], param_1[1]),
+    #     bins_1, count_1,
+    #     p0 = [10, 0.2, 9.45, 2, 0.4, 9.45], maxfev = 4000)
         
     
     # print(param_1,'\n' ,param_1_d)
@@ -124,8 +146,8 @@ def main():
     # param_3, cov_3 = curve_fit(gauss, bins_3, count_3_clean, p0 = [2500, 0.3,10.35])
     
     count_1_fit = gauss(bins_1, param_1[0], param_1[1], param_1[2]) 
-    count_1_d_fit = double_gauss(bins_1, param_1_d[0], param_1_d[1], param_1_d[2],
-                    param_1_d[3], param_1_d[4], param_1_d[5], param_1[0], param_1[1]) 
+    # count_1_d_fit = double_gauss(bins_1, param_1_d[0], param_1_d[1], param_1_d[2],
+    #                 param_1_d[3], param_1_d[4], param_1_d[5], param_1[0], param_1[1]) 
     # count_2_fit = gauss(bins_2, param_2[0], param_2[1], param_2[2]) + decay(
     #     np.array(bins_2), param_back[0], param_back[1])
     # count_3_fit = gauss(bins_3, param_3[0], param_3[1], param_3[2]) + decay(
@@ -151,15 +173,15 @@ def main():
     
     #Residual
     
-    res_1 = count_1_fit - count_1
-    res_1_d = count_1_d_fit - count_1
-    res_1_d_sq = np.square(res_1_d)
-    res_1_sq = np.square(res_1)
+    # res_1 = count_1_fit - count_1
+    # res_1_d = count_1_d_fit - count_1
+    # res_1_d_sq = np.square(res_1_d)
+    # res_1_sq = np.square(res_1)
     # res_2 = count_2_fit - count_2
     # res_3 = count_3_fit - count_3
     
-    r_sq_1 = 1 - (np.sum((count_1- count_1_fit)**2)/(np.sum((count_1-np.mean(count_1))**2)))
-    r_sq_1_d = 1 - (np.sum((count_1- count_1_d_fit)**2)/(np.sum((count_1-np.mean(count_1))**2)))
+    # r_sq_1 = 1 - (np.sum((count_1- count_1_fit)**2)/(np.sum((count_1-np.mean(count_1))**2)))
+    # r_sq_1_d = 1 - (np.sum((count_1- count_1_d_fit)**2)/(np.sum((count_1-np.mean(count_1))**2)))
     
     # print(f'single = {r_sq_1}\ndouble = {r_sq_1_d}\n{r_sq_1_d - r_sq_1}')
     # beta = 2
@@ -197,9 +219,9 @@ def main():
     # plt.show()
     # plt.clf()
 
-a = datetime.datetime.now()    
+# a = datetime.datetime.now()    
 main()
-b = datetime.datetime.now() - a
-print(b)
+# b = datetime.datetime.now() - a
+# print(b)
 
 
