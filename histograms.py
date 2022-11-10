15b437a40f9bc6be32933703d51097fc4e06b877
aqnemo32 <s1973088@ed.ac.uk>
2022-11-10 10:34:09 +0000

    Update histograms.py

--- histograms.py
+++ histograms.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 xmass = np.load('xmass.npy')
 mom_pair = np.load('mom_pair.npy')
-mom_tran_1 = np,load('mom_tran_1.npy')
+mom_tran_1 = np.load('mom_tran_1.npy')
 mom_tran_2 = np.load('mom_tran_2.npy')
 mom_tran_pair = np.load('mom_tran_pain.npy')
 rap = np.load('rap.npy')
