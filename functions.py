580975fd3c7817dc88d84f3cdcb161958becf9cb
aqnemo32 <s1973088@ed.ac.uk>
2022-11-10 23:18:23 +0000

    Update functions.py

--- functions.py
+++ functions.py
@@ -13,7 +13,6 @@ from scipy.special import erf
 def gauss(x, a, mu, sig):
     '''
     
-
     Parameters
     ----------
     x : TYPE
@@ -33,8 +32,26 @@ def gauss(x, a, mu, sig):
     '''
     return a*np.exp(-np.square(x-mu)/(2*np.square(sig)))
 
-def crystalball(x, alpha, n, xbar, sigma):
+def crystalball(x, alpha, n, mu, sig):
     '''
+    
+    Parameters
+    ----------
+    x : TYPE
+        DESCRIPTION.
+    alpha : TYPE
+        DESCRIPTION.
+    n : TYPE
+        DESCRIPTION.
+    mu : TYPE
+        DESCRIPTION.
+    sig : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    TYPE
+        DESCRIPTION.
     '''
     a = np.absolute(alpha)
     A = ((n/a)**n)*np.exp(-((a**2)/2))
@@ -42,22 +59,44 @@ def crystalball(x, alpha, n, xbar, sigma):
     B = n/a - a
     C = n/a * 1/(n-1) * np.exp(-((a**2)/2))
     D = np.sqrt(pi/2)*(1+erf(a/np.sqrt(2)))
-    N = 1/(sigma*(C + D))
+    N = 1/(sig*(C + D))
 
     out  = []
     
     for i in x:
     
-        if (i-xbar)/sigma > -alpha:
-             out.append(N*np.exp(-np.square(i-xbar)/(2*np.square(sigma))))
+        if (i-mu)/sig > -alpha:
+             out.append(N*np.exp(-np.square(i-mu)/(2*np.square(sig))))
         
         else:
-            out.append(N*A*(B-((i-xbar)/sigma))**(-n))
+            out.append(N*A*(B-((i-mu)/sig))**(-n))
             
     return np.array(out)
 
-def double_crystalball(x, alpha1, alpha2, n1, n2, xbar, sigma):
+def double_crystalball(x, alpha1, alpha2, n1, n2, mu, sig):
     '''
+    
+    Parameters
+    ----------
+    x : TYPE
+        DESCRIPTION.
+    alpha1 : TYPE
+        DESCRIPTION.
+    alpha1 : TYPE
+        DESCRIPTION.
+    n1 : TYPE
+        DESCRIPTION.
+    n2 : TYPE
+        DESCRIPTION.
+    mu : TYPE
+        DESCRIPTION.
+    sig : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    TYPE
+        DESCRIPTION.
     '''
     out = []
     a1 = np.absolute(alpha1)
@@ -67,24 +106,24 @@ def double_crystalball(x, alpha1, alpha2, n1, n2, xbar, sigma):
     B1 = n1/a1 - a1
     C1= n1/a1 * 1/(n1-1) * np.exp(-((a1**2)/2))
     D1 = np.sqrt(pi/2)*(1+erf(a1/np.sqrt(2)))
-    N1 = 1/(sigma*(C1 + D1))
+    N1 = 1/(sig*(C1 + D1))
 
     A2 = ((n2/a2)**n2)*np.exp(-((a2**2)/2))
     B2 = n2/a2 - a2
     C2= n2/a2 * 1/(n2-1) * np.exp(-((a2**2)/2))
     D2 = np.sqrt(pi/2)*(1+erf(a2/np.sqrt(2)))
-    N2 = 1/(sigma*(C2 + D2))
+    N2 = 1/(sig*(C2 + D2))
     
     N = (N1 + N2)/2
 
     for i in x:
-        if (i-xbar)/sigma < -a1:
-            out.append(A1*(B1-((i-xbar)/sigma))**(-n1))
-        elif (i-xbar)/sigma > -a1 and (i-xbar)/sigma < a2:
-            out.append(np.exp(-np.square(i-xbar)/(2*np.square(sigma))))
-        elif (i-xbar)/sigma > a2:
-            out.append(A2*(B2-((i-xbar)/sigma))**(-n2))
-            # (i-xbar)/sigma > a2
+        if (i-mu)/sig < -a1:
+            out.append(A1*(B1-((i-mu)/sig))**(-n1))
+        elif (i-mu)/sig > -a1 and (i-mu)/sig < a2:
+            out.append(np.exp(-np.square(i-mu)/(2*np.square(sig))))
+        elif (i-mu)/sig > a2:
+            out.append(A2*(B2-((i-mu)/sig))**(-n2))
+            # (i-mu)/sig > a2
 
     
     return np.array(out)
@@ -93,7 +132,6 @@ def double_crystalball(x, alpha1, alpha2, n1, n2, xbar, sigma):
 
 def decay(x, a, b):
     '''
-    
 
     Parameters
     ----------
@@ -115,7 +153,6 @@ def decay(x, a, b):
 
 def chi_sq(fit,data):
     '''
-    
 
     Parameters
     ----------
@@ -134,7 +171,6 @@ def chi_sq(fit,data):
 
 def peak_split(x, y, sig, mu):
     '''
-    
 
     Parameters
     ----------
