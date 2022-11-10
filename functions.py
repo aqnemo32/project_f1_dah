c50062372deaee3650632900a3919ea8289bb919
aqnemo32 <s1973088@ed.ac.uk>
2022-11-10 22:27:09 +0000

    work on double crystal ball

--- functions.py
+++ functions.py
@@ -9,6 +9,7 @@ import numpy as np
 from scipy.constants import pi
 from scipy.special import erf
 
+
 def gauss(x, a, sig, mu):
     '''
     
@@ -33,6 +34,8 @@ def gauss(x, a, sig, mu):
     return a*np.exp(-np.square(x-mu)/(2*np.square(sig)))
 
 def crystalball(x, alpha, n, xbar, sigma):
+    '''
+    '''
     a = np.absolute(alpha)
     A = ((n/a)**n)*np.exp(-((a**2)/2))
     
@@ -52,6 +55,41 @@ def crystalball(x, alpha, n, xbar, sigma):
             out.append(N*A*(B-((i-xbar)/sigma))**(-n))
             
     return np.array(out)
+
+def double_crystalball(x, alpha1, alpha2, n1, n2, xbar, sigma):
+    '''
+    '''
+    out = []
+    a1 = np.absolute(alpha1)
+    a2 = np.absolute(alpha2)
+
+    A1 = ((n1/a1)**n1)*np.exp(-((a1**2)/2))
+    B1 = n1/a1 - a1
+    C1= n1/a1 * 1/(n1-1) * np.exp(-((a1**2)/2))
+    D1 = np.sqrt(pi/2)*(1+erf(a1/np.sqrt(2)))
+    N1 = 1/(sigma*(C1 + D1))
+
+    A2 = ((n2/a2)**n2)*np.exp(-((a2**2)/2))
+    B2 = n2/a2 - a2
+    C2= n2/a2 * 1/(n2-1) * np.exp(-((a2**2)/2))
+    D2 = np.sqrt(pi/2)*(1+erf(a2/np.sqrt(2)))
+    N2 = 1/(sigma*(C2 + D2))
+    
+    N = (N1 + N2)/2
+
+    for i in x:
+        if (i-xbar)/sigma < -a1:
+            out.append(A1*(B1-((i-xbar)/sigma))**(-n1))
+        elif (i-xbar)/sigma < -a1 and (i-xbar)/sigma > a2:
+            out.append(np.exp(-np.square(i-xbar)/(2*np.square(sigma))))
+        else:
+            out.append(A2*(B2-((i-xbar)/sigma))**(-n2))
+            # (i-xbar)/sigma > a2
+
+    
+    return np.array(out)
+
+
 
 def decay(x, a, b):
     '''
