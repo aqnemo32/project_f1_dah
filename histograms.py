918481c0348bec11b9860ec510077f7eda76b0ff
aqnemo32 <s1973088@ed.ac.uk>
2022-11-15 14:53:37 +0000

    Update histograms.py

--- histograms.py
+++ histograms.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from functions import freedman
 
 
 xmass = np.load('xmass.npy')
@@ -9,30 +10,42 @@ mom_tran_2 = np.load('mom_tran_2.npy')
 mom_tran_pair = np.load('mom_tran_pair.npy')
 rap = np.load('rap.npy')
 
-plt.hist(xmass, color = 'k' ,bins = 600, histtype= 'bar', density = False)
+_, n_bins_xmass = np.modf((np.max(xmass)-np.min(xmass))/freedman(xmass))
+
+plt.hist(xmass, color = 'k' ,bins = int(n_bins_xmass), histtype= 'bar', density = False)
 plt.title('xmass')
 plt.savefig('/Users/achillequarante/Desktop/dah_graphs_project/mass_hist', format = 'png')
 plt.clf()
 
-plt.hist(mom_pair, color = 'k' ,bins = 100, range = [np.min(mom_pair), np.max(mom_pair)], histtype= 'bar', density = False)
+_, n_bins_mom_pair = np.modf((np.max(mom_pair)-np.min(mom_pair))/freedman(mom_pair))
+
+plt.hist(mom_pair, color = 'k' ,bins = int(n_bins_mom_pair), range = [np.min(mom_pair), np.max(mom_pair)], histtype= 'bar', density = False)
 plt.title('Muon pair momentum (CoM momentum)')
 plt.savefig('/Users/achillequarante/Desktop/dah_graphs_project/mom_pair', format = 'png')
 plt.clf()
 
-plt.hist(mom_tran_1, color = 'k' ,bins = 200, histtype= 'bar', density = False)
+_, n_bins_mom_tran_1 = np.modf((np.max(mom_tran_1)-np.min(mom_tran_1))/freedman(mom_tran_1))
+
+plt.hist(mom_tran_1, color = 'k' ,bins = int(n_bins_mom_tran_1), histtype= 'bar', density = False)
 plt.title('Transverse momentum 1')
 plt.savefig('/Users/achillequarante/Desktop/dah_graphs_project/mom_tran_hist', format = 'png')
 plt.clf()
 
-plt.hist(mom_tran_2, color = 'k' ,bins = 200, histtype= 'bar', density = False)
+_, n_bins_mom_tran_2 = np.modf((np.max(mom_tran_2)-np.min(mom_tran_2))/freedman(mom_tran_2))
+
+plt.hist(mom_tran_2, color = 'k' ,bins = int(n_bins_mom_tran_2), histtype= 'bar', density = False)
 plt.title('Transverse momentum 2')
 plt.savefig('/Users/achillequarante/Desktop/dah_graphs_project/mom_tran_2_hist', format = 'png')
 plt.clf()
 
-plt.hist(mom_tran_pair, color = 'k' ,bins = 200, histtype= 'bar', density = False)
+_, n_bins_mom_tran_pair = np.modf((np.max(mom_tran_pair)-np.min(mom_tran_pair))/freedman(mom_tran_pair))
+
+plt.hist(mom_tran_pair, color = 'k' ,bins = int(n_bins_mom_tran_pair), histtype= 'bar', density = False)
 plt.title('Transverse momentum of muon pair (CoM transverse momentum)')
 plt.savefig('/Users/achillequarante/Desktop/dah_graphs_project/mom_tran_1_hist', format = 'png')
 plt.clf()
+
+_, n_bins_rap = np.modf((np.max(rap)-np.min(rap))/freedman(rap))
 
 plt.hist(rap, color = 'k' ,bins = 600, histtype= 'bar', density = False)
 plt.title('Rapidity')
